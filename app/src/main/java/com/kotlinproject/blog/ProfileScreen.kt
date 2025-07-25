b5dc9568e95071751c0de7bc3b44b7b705ff8c43
chapaiprakriti <prakriti9745906712@gmail.com>
2025-07-25 22:57:04 +0545

    Initial commit - Blog project

--- app/src/main/java/com/kotlinproject/blog/ProfileScreen.kt
+++ app/src/main/java/com/kotlinproject/blog/ProfileScreen.kt
@@ -61,6 +61,7 @@ fun ProfileScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
+            //white
             .padding(top = 32.dp, start = 16.dp, end = 16.dp, bottom = 0.dp)
     ) {
         Column(modifier = Modifier.fillMaxSize()) {
